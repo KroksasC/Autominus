07216a7f9927d0476f8b3c097f77c3d2f4652fc2
pyuus <pijus.bataitis@gmail.com>
2025-03-25 00:57:52 +0200

    Pakeičiau savo testa + Fausto testas nes jam nesigauna pushint

--- Auto.tests/Test1.cs
+++ Auto.tests/Test1.cs
@@ -31,23 +31,6 @@ namespace Auto.Tests
             return context;
         }
 
-        private ModelsContext GetDbCarContext()
-        {
-            var options = new DbContextOptionsBuilder<ModelsContext>()
-                .UseInMemoryDatabase(databaseName: "TestDb")
-                .Options;
-
-            var context = new ModelsContext(options);
-
-            // Seed the database with a test car
-            context.Cars.Add(new Car
-            {
-                Id = 1,
-            });
-            context.SaveChanges();
-
-            return context;
-        }
 
         [TestMethod]
         public async Task GetUser_ShouldReturnNotFound_WhenUserDoesNotExist()
@@ -62,21 +45,63 @@ namespace Auto.Tests
             // Assert
             Assert.IsInstanceOfType(result.Result, typeof(NotFoundResult));  // Should return 404
         }
+    }
+    [TestClass]
+    public sealed class CarControllerTests
+    {
+        private ModelsContext GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ModelsContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new ModelsContext(options);
+
+            context.Cars.Add(new Car
+            {
+                Id = 1,
+                Brand = "1",
+                Model = "1"
+
+            });
+            context.SaveChanges();
+
+            return context;
+        }
+
         [TestMethod]
-        public async Task GetCar_ShouldNotReturnNull_WhenCarExists()
+        public async Task GetCar_ShouldReturnNull_WhenCarDoesntExist()
         {
             // Arrange
-            var context = GetDbCarContext();
+            var context = GetDbContext();
             var controller = new CarController(context);
 
             // Act
+            var result = await controller.GetCar(999);
+            var okResult = result.Result as OkObjectResult;
+            var car = okResult?.Value as Car;
+
+            // Assert
+            Assert.IsInstanceOfType(result.Result, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public async Task GetCar_ShouldReturnCar_WhenCarExists()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var testCar = new Car { Id = 2, Brand = "1", Model = "1" };
+            context.Cars.Add(testCar);
+            context.SaveChanges();
+
             var result = await controller.GetCar(1);
             var okResult = result.Result as OkObjectResult;
             var car = okResult?.Value as Car;
 
-
-            // Assert
             Assert.IsNotNull(result.Value);
+            Assert.AreEqual("1", result.Value.Brand);
+            Assert.AreEqual("1", result.Value.Model);
         }
     }
 }
