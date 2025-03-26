958997e3762f6bb82e7ef1396fa596c55d3d7c63
pyuus <pijus.bataitis@gmail.com>
2025-03-26 17:27:19 +0200

    Pridėta delete lentelė ir  veikia skelbimo ištrynimas

--- autominus.client/src/components/EditDeleteButtons.jsx
+++ autominus.client/src/components/EditDeleteButtons.jsx
@@ -2,25 +2,36 @@
 import DeleteImg from "../images/delete.png";
 import "../Styles/EditDeleteButtons.css"
 import { Link } from "react-router-dom"
+import DeleteConfirmation from "../components/DeleteConfirmation"; 
+import { useState } from "react";
+
 function EditDeleteButtons() {
+    const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
     return (
         <div>
+            
             <table className="EDButtons">
                 <tbody>
                     <tr>
                         <td>
                             <Link to={`/`} style={{ textDecoration: 'none' }}>
-                                <img className="Edit" src={EditImg} alt="Redaguoti"></img>
+                                <img className="Edit" src={EditImg} alt="Redaguoti" />
                             </Link>
                         </td>
                         <td>
-                            <Link to={`/`} style={{ textDecoration: 'none' }}>
+                            <button
+                                style={{ background: "none", border: "none", padding: 0, cursor: "pointer" }}
+                                onClick={() => setShowDeleteConfirmation(true)}
+                            >
+                                
                                 <img className="Delete" src={DeleteImg} alt="Pašalinti"></img>
-                            </Link>
+                            </button>
                         </td>
                     </tr>
                 </tbody>
             </table>
+            {showDeleteConfirmation && (
+                <DeleteConfirmation onClose={() => setShowDeleteConfirmation(false)} />)}
         </div>
     );
 }
