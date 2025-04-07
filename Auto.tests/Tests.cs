acf806207aef4c882174a1093f2b400f44438c23
Benazzz <178609468+Benazzz@users.noreply.github.com>
2025-04-07 19:16:04 +0300

    Added tests

--- Auto.tests/Tests.cs
+++ Auto.tests/Tests.cs
@@ -74,12 +74,34 @@ namespace Auto.Tests
             context.Cars.Add(new Car
             {
                 Id = 1,
-                Brand = "1",
-                Model = "1"
-
+                Brand = "Toyota",
+                Model = "Corolla",
+                Year = 2020,
+                Price = 20000,
+                CreatedAt = DateTime.UtcNow.AddDays(-1)
             });
-            context.SaveChanges();
 
+            context.Cars.Add(new Car
+            {
+                Id = 2,
+                Brand = "Toyota",
+                Model = "Camry",
+                Year = 2021,
+                Price = 25000,
+                CreatedAt = DateTime.UtcNow.AddDays(-3)
+            });
+
+            context.Cars.Add(new Car
+            {
+                Id = 3,
+                Brand = "Honda",
+                Model = "Civic",
+                Year = 2019,
+                Price = 18000,
+                CreatedAt = DateTime.UtcNow.AddDays(-10)
+            });
+
+            context.SaveChanges();
             return context;
         }
 
@@ -106,11 +128,425 @@ namespace Auto.Tests
             var controller = new CarController(context);
 
             var result = await controller.GetCar(1);
-            var okResult = result.Result as OkObjectResult;
 
             Assert.IsNotNull(result.Value);
-            Assert.AreEqual("1", result.Value.Brand);
-            Assert.AreEqual("1", result.Value.Model);
+            Assert.AreEqual("Toyota", result.Value.Brand);
+            Assert.AreEqual("Corolla", result.Value.Model);
+        }
+
+        [TestMethod]
+        public async Task GetCarCount_ShouldReturnCorrectCount()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var result = await controller.GetCarCount();
+
+            Assert.AreEqual(3, result.Value);
+        }
+
+        [TestMethod]
+        public async Task SearchCars_ShouldReturnBadRequest_WhenQueryEmpty()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var result = await controller.SearchCars("");
+
+            Assert.IsInstanceOfType(result.Result, typeof(BadRequestObjectResult));
+        }
+
+        [TestMethod]
+        public async Task GetCarsOrderedByYear_ShouldReturnAscendingByDefault()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var result = await controller.GetCarsOrderedByYear();
+            var cars = result.Value as IEnumerable<Car>;
+
+            Assert.AreEqual(2019, cars?.First().Year);
+            Assert.AreEqual(2021, cars?.Last().Year);
+        }
+
+        [TestMethod]
+        public async Task GetCarsOrderedByYear_ShouldReturnDescending_WhenRequested()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var result = await controller.GetCarsOrderedByYear(true);
+            var cars = result.Value as IEnumerable<Car>;
+
+            Assert.AreEqual(2021, cars?.First().Year);
+            Assert.AreEqual(2019, cars?.Last().Year);
+        }
+
+        [TestMethod]
+        public async Task GetRecentCars_ShouldReturnRecentCarsOnly()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var result = await controller.GetRecentCars(5); // last 5 days
+            var cars = result.Value as IEnumerable<Car>;
+
+            Assert.AreEqual(2, cars?.Count());
+        }
+
+        [TestMethod]
+        public async Task AdvancedSearchCars_ShouldFilterByMultipleParameters()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var result = await controller.AdvancedSearchCars(brand: "Toyota", model: "Camry", year: 2021, minPrice: null, maxPrice: null);
+            var okResult = result.Result as OkObjectResult;
+            var cars = okResult?.Value as List<Car>;
+
+            Assert.AreEqual(1, cars?.Count);
+            Assert.AreEqual("Camry", cars?[0].Model);
+        }
+
+        [TestMethod]
+        public async Task GetRandomCar_ShouldReturnValidCar()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var result = await controller.GetRandomCar();
+
+            Assert.IsNotNull(result.Value);
+        }
+
+        [TestMethod]
+        public async Task GetMostExpensiveCars_ShouldReturnTopPricedCars()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var result = await controller.GetMostExpensiveCars(2);
+            var cars = result.Value as IEnumerable<Car>;
+
+            Assert.AreEqual(2, cars?.Count());
+            Assert.AreEqual(25000, cars?.First().Price);
+        }
+
+        [TestMethod]
+        public async Task GetCarsByYear_ShouldFilterCorrectly()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var result = await controller.GetCarsByYear(2020);
+            var cars = result.Value as IEnumerable<Car>;
+
+            Assert.AreEqual(1, cars?.Count());
+            Assert.AreEqual("Corolla", cars?.First().Model);
+        }
+
+        [TestMethod]
+        public async Task UpdateCarPrice_ShouldUpdatePrice()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var newPrice = 30000m;
+            var result = await controller.UpdateCarPrice(1, newPrice);
+
+            Assert.IsInstanceOfType(result, typeof(NoContentResult));
+
+            var updatedCar = await context.Cars.FindAsync(1);
+            Assert.AreEqual(newPrice, updatedCar?.Price);
+        }
+
+        [TestMethod]
+        public async Task GetSimilarCars_ShouldReturnRelatedCars()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var result = await controller.GetSimilarCars(1);
+            var cars = result.Value as IEnumerable<Car>;
+
+            Assert.IsTrue(cars?.Any());
+            Assert.IsFalse(cars?.Any(c => c.Id == 1));
+        }
+
+        [TestMethod]
+        public async Task AdvancedSearchCars_ShouldReturnAllCars_WhenNoFiltersApplied()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var result = await controller.AdvancedSearchCars(null, null, null, null, null);
+            var okResult = result.Result as OkObjectResult;
+            var cars = okResult?.Value as List<Car>;
+
+            Assert.AreEqual(3, cars?.Count);
+        }
+
+        [TestMethod]
+        public async Task GetRandomCar_ShouldReturnNotFound_WhenNoCarsExist()
+        {
+            var options = new DbContextOptionsBuilder<ModelsContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            var emptyContext = new ModelsContext(options);
+
+            var controller = new CarController(emptyContext);
+            var result = await controller.GetRandomCar();
+
+            Assert.IsInstanceOfType(result.Result, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public async Task GetMostExpensiveCars_ShouldReturnLimitedResults_WhenCountSpecified()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var result = await controller.GetMostExpensiveCars(1);
+            var cars = result.Value as IEnumerable<Car>;
+
+            Assert.AreEqual(1, cars?.Count());
+        }
+
+        [TestMethod]
+        public async Task GetCarsByYear_ShouldReturnEmptyList_WhenNoCarsForYearExist()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var result = await controller.GetCarsByYear(1990);
+            var cars = result.Value as IEnumerable<Car>;
+
+            Assert.AreEqual(0, cars?.Count());
+        }
+
+        [TestMethod]
+        public async Task UpdateCarPrice_ShouldReturnNotFound_WhenCarDoesntExist()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var result = await controller.UpdateCarPrice(999, 30000m);
+
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public async Task GetSimilarCars_ShouldReturnEmptyList_WhenNoSimilarCarsExist()
+        {
+            // Add a unique car that has no similar models
+            var options = new DbContextOptionsBuilder<ModelsContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            var uniqueContext = new ModelsContext(options);
+            uniqueContext.Cars.Add(new Car
+            {
+                Id = 1,
+                Brand = "UniqueBrand",
+                Model = "UniqueModel",
+                Year = 1990
+            });
+            uniqueContext.SaveChanges();
+
+            var controller = new CarController(uniqueContext);
+            var result = await controller.GetSimilarCars(1);
+            var cars = result.Value as IEnumerable<Car>;
+
+            Assert.AreEqual(0, cars?.Count());
+        }
+
+        [TestMethod]
+        public async Task AdvancedSearchCars_ShouldHandlePriceRangeCorrectly()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var result = await controller.AdvancedSearchCars(null, null, null, 19000, 21000);
+            var okResult = result.Result as OkObjectResult;
+            var cars = okResult?.Value as List<Car>;
+
+            Assert.AreEqual(1, cars?.Count);
+            Assert.AreEqual("Corolla", cars?[0].Model);
+        }
+
+        [TestMethod]
+        public async Task GetRecentCars_ShouldHandleZeroDaysParameter()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var result = await controller.GetRecentCars(0);
+            var cars = result.Value as IEnumerable<Car>;
+
+            Assert.AreEqual(0, cars?.Count());
+        }
+
+        [TestMethod]
+        public async Task GetMostExpensiveCars_ShouldReturnAll_WhenCountExceedsTotal()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var result = await controller.GetMostExpensiveCars(10);
+            var cars = result.Value as IEnumerable<Car>;
+
+            Assert.AreEqual(3, cars?.Count());
+        }
+
+        [TestMethod]
+        public async Task GetCarsByYear_ShouldHandleFutureYear()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var result = await controller.GetCarsByYear(2050);
+            var cars = result.Value as IEnumerable<Car>;
+
+            Assert.AreEqual(0, cars?.Count());
+        }
+
+        [TestMethod]
+        public async Task GetSimilarCars_ShouldPrioritizeSameBrand()
+        {
+            // Add more test data
+            var options = new DbContextOptionsBuilder<ModelsContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            var context = new ModelsContext(options);
+
+            context.Cars.Add(new Car { Id = 1, Brand = "Toyota", Model = "A", Year = 2020 });
+            context.Cars.Add(new Car { Id = 2, Brand = "Toyota", Model = "B", Year = 2019 });
+            context.Cars.Add(new Car { Id = 3, Brand = "Honda", Model = "A", Year = 2020 });
+            context.SaveChanges();
+
+            var controller = new CarController(context);
+            var result = await controller.GetSimilarCars(1);
+            var cars = result.Value as IEnumerable<Car>;
+
+            // Should prefer Toyota models first
+            Assert.AreEqual("Toyota", cars?.First().Brand);
+        }
+
+        [TestMethod]
+        public async Task GetCarCount_ShouldReturnZero_AfterDeletingAllCars()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            // Delete all cars
+            foreach (var car in context.Cars)
+            {
+                context.Cars.Remove(car);
+            }
+            context.SaveChanges();
+
+            var result = await controller.GetCarCount();
+
+            Assert.AreEqual(0, result.Value);
+        }
+
+        [TestMethod]
+        public async Task AdvancedSearchCars_ShouldHandleOnlyMinPrice()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var result = await controller.AdvancedSearchCars(null, null, null, 22000, null);
+            var okResult = result.Result as OkObjectResult;
+            var cars = okResult?.Value as List<Car>;
+
+            Assert.AreEqual(1, cars?.Count);
+            Assert.AreEqual("Camry", cars?[0].Model);
+        }
+
+        [TestMethod]
+        public async Task AdvancedSearchCars_ShouldHandleOnlyMaxPrice()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var result = await controller.AdvancedSearchCars(null, null, null, null, 19000);
+            var okResult = result.Result as OkObjectResult;
+            var cars = okResult?.Value as List<Car>;
+
+            Assert.AreEqual(1, cars?.Count);
+            Assert.AreEqual("Civic", cars?[0].Model);
+        }
+
+        [TestMethod]
+        public async Task GetRandomCar_ShouldReturnDifferentCars_OnMultipleCalls()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var results = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                var result = await controller.GetRandomCar();
+                results.Add(result.Value.Id);
+            }
+
+            // Verify we got at least 2 different cars in 10 attempts
+            Assert.IsTrue(results.Distinct().Count() > 1);
+        }
+
+        [TestMethod]
+        public async Task GetMostExpensiveCars_ShouldReturnInCorrectOrder()
+        {
+            var context = GetDbContext();
+            var controller = new CarController(context);
+
+            var result = await controller.GetMostExpensiveCars(3);
+            var cars = result.Value as List<Car>;
+
+            Assert.IsTrue(cars?[0].Price >= cars?[1].Price);
+            Assert.IsTrue(cars?[1].Price >= cars?[2].Price);
+        }
+
+        [TestMethod]
+        public async Task GetCarsByYear_ShouldHandleMultipleCarsSameYear()
+        {
+            // Add test data with multiple cars from same year
+            var options = new DbContextOptionsBuilder<ModelsContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            var context = new ModelsContext(options);
+
+            context.Cars.Add(new Car { Id = 1, Brand = "A", Model = "A", Year = 2020 });
+            context.Cars.Add(new Car { Id = 2, Brand = "B", Model = "B", Year = 2020 });
+            context.SaveChanges();
+
+            var controller = new CarController(context);
+            var result = await controller.GetCarsByYear(2020);
+            var cars = result.Value as IEnumerable<Car>;
+
+            Assert.AreEqual(2, cars?.Count());
+        }
+
+        [TestMethod]
+        public async Task GetSimilarCars_ShouldLimitResultsToFive()
+        {
+            // Add test data with many similar cars
+            var options = new DbContextOptionsBuilder<ModelsContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            var context = new ModelsContext(options);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                context.Cars.Add(new Car { Id = i, Brand = "Toyota", Model = $"Model{i}", Year = 2020 });
+            }
+            context.SaveChanges();
+
+            var controller = new CarController(context);
+            var result = await controller.GetSimilarCars(1);
+            var cars = result.Value as IEnumerable<Car>;
+
+            Assert.AreEqual(5, cars?.Count());
         }
     }
 }
--- Autominus.Server/Controllers/CarController.cs
+++ Autominus.Server/Controllers/CarController.cs
@@ -113,15 +113,6 @@ namespace Autominus.Server.Controllers
             return results;
         }
 
-        [HttpGet("brands")]
-        public async Task<ActionResult<IEnumerable<string>>> GetDistinctBrands()
-        {
-            return await _context.Cars
-                .Select(c => c.Brand)
-                .Distinct()
-                .ToListAsync();
-        }
-
         [HttpGet("ordered-by-year")]
         public async Task<ActionResult<IEnumerable<Car>>> GetCarsOrderedByYear([FromQuery] bool desc = false)
         {
@@ -130,17 +121,6 @@ namespace Autominus.Server.Controllers
                 : _context.Cars.OrderBy(c => c.Year);
 
             return await cars.Include(c => c.User).ToListAsync();
-        }
-
-        [HttpGet("stats/make-counts")]
-        public async Task<ActionResult<IEnumerable<object>>> GetCarBrandCounts()
-        {
-            var stats = await _context.Cars
-                .GroupBy(c => c.Brand)
-                .Select(g => new { Make = g.Key, Count = g.Count() })
-                .ToListAsync();
-
-            return Ok(stats);
         }
 
         [HttpGet("recent")]
@@ -155,7 +135,7 @@ namespace Autominus.Server.Controllers
 
         [HttpGet("search-advanced")]
         public async Task<ActionResult<IEnumerable<Car>>> AdvancedSearchCars(
-            [FromQuery] string make,
+            [FromQuery] string brand,
             [FromQuery] string model,
             [FromQuery] int? year,
             [FromQuery] decimal? minPrice,
@@ -163,8 +143,8 @@ namespace Autominus.Server.Controllers
         {
             var query = _context.Cars.AsQueryable();
 
-            if (!string.IsNullOrEmpty(make))
-                query = query.Where(c => c.Brand.Contains(make));
+            if (!string.IsNullOrEmpty(brand))
+                query = query.Where(c => c.Brand.Contains(brand));
 
             if (!string.IsNullOrEmpty(model))
                 query = query.Where(c => c.Model.Contains(model));
@@ -180,25 +160,6 @@ namespace Autominus.Server.Controllers
 
             var result = await query.Include(c => c.User).ToListAsync();
             return Ok(result);
-        }
-
-        [HttpGet("price-range")]
-        public async Task<ActionResult<object>> GetPriceRange()
-        {
-            var minPrice = await _context.Cars.MinAsync(c => c.Price);
-            var maxPrice = await _context.Cars.MaxAsync(c => c.Price);
-            var avgPrice = await _context.Cars.AverageAsync(c => c.Price);
-
-            return new { MinPrice = minPrice, MaxPrice = maxPrice, AveragePrice = avgPrice };
-        }
-
-        [HttpGet("year-range")]
-        public async Task<ActionResult<object>> GetYearRange()
-        {
-            var minYear = await _context.Cars.MinAsync(c => c.Year);
-            var maxYear = await _context.Cars.MaxAsync(c => c.Year);
-
-            return new { OldestYear = minYear, NewestYear = maxYear };
         }
 
         [HttpGet("random")]
@@ -240,24 +201,6 @@ namespace Autominus.Server.Controllers
                 .ToListAsync();
         }
 
-        [HttpGet("price-stats-by-brand")]
-        public async Task<ActionResult<IEnumerable<object>>> GetPriceStatsByBrand()
-        {
-            var stats = await _context.Cars
-                .GroupBy(c => c.Brand)
-                .Select(g => new
-                {
-                    Brand = g.Key,
-                    Count = g.Count(),
-                    AveragePrice = g.Average(c => c.Price),
-                    MinPrice = g.Min(c => c.Price),
-                    MaxPrice = g.Max(c => c.Price)
-                })
-                .ToListAsync();
-
-            return Ok(stats);
-        }
-
         [HttpPatch("{id}/price")]
         public async Task<IActionResult> UpdateCarPrice(int id, [FromBody] decimal newPrice)
         {
@@ -291,18 +234,5 @@ namespace Autominus.Server.Controllers
                 .Take(5)
                 .ToListAsync();
         }
-
-        [HttpGet("count-by-year")]
-        public async Task<ActionResult<IEnumerable<object>>> GetCountByYear()
-        {
-            var stats = await _context.Cars
-                .GroupBy(c => c.Year)
-                .Select(g => new { Year = g.Key, Count = g.Count() })
-                .OrderBy(x => x.Year)
-                .ToListAsync();
-
-            return Ok(stats);
-        }
-
     }
 }
